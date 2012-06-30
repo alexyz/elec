96b23da5a12cb5f358ad04c7417ee5bfef369733
Your Name <alexslack56@hotmail.com>
2012-06-30 08:12:41 +0100

    rpc changes

---
+++ src/el/serv/ClientCommands.java
@@ -0,0 +1,35 @@
+package el.serv;
+
+/**
+ * Commands sent from server to client
+ */
+public interface ClientCommands {
+
+	/** tell client new server time */
+	public void setTime(long time);
+	/** tell client with id to enter */
+	public void playerEntered(int id, int freq, float x, float y, boolean msg);
+	/** tell client to spectate */
+	public void playerSpectated(int id);
+	/** tell client its identifier */
+	public void setId(int id, String name);
+	/** tell client to update the given object */
+	public void updateFgObject(int id, String data);
+	/** tell client to discard any existing map and use given map data */
+	public void setMapData(String data);
+	/** tell client to update map tile */
+	public void setMapTile(int x, int y, int action);
+	/** tell client to add bullet */
+	public void addBullet(int freq, String data);
+	/** send chat to client */
+	public void addMsg(int id, String msg);
+	/** tell client someone has connected - should send update in response */
+	public void playerConnected(int id, String name);
+	/** tell client someone has exited */
+	public void playerExited(int id);
+	/** tell client the bullet has exploded */
+	public void bulletExploded(int transid);
+	/** tell client someone was killed */
+	public void playerKilled(int id, int killerId, float x, float y);
+	
+}
---
+++ src/el/serv/ServerCommands.java
@@ -0,0 +1,27 @@
+package el.serv;
+
+/**
+ * Asynchronous commands sent from client to server.
+ */
+public interface ServerCommands {
+
+	/** ask server if we can enter */
+	public void enterReq();
+	/** tell server we are going to spectate */
+	public void spec();
+	/** update details of players ship */
+	public void update(String data);
+	/** update map tile request */
+	public void setMapTile(int x, int y, int action);
+	/** fire transient */
+	public void addBullet(String data);
+	/** send text message to server */
+	public void addMsg(String msg);
+	/** send client name to server (should send ID back) */
+	public void setName(String name);
+	/** send hit to server */
+	public void playerHit(int transid);
+	/** current players ship was killed */
+	public void playerKilled(int killedId, float x, float y);
+	
+}
